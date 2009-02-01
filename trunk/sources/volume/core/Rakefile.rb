a5e1cbd0a038e2a231170b13e4dce66bcdc05e5c
jorge.manrubia <jorge.manrubia@ade3d05c-f855-0410-b20e-87ffbe595ca4>
2009-02-01 23:40:54 +0000

    - Added tasks to generate instructions for authors
    - Corrected emindspaper.cls to generate first page signature only if volume compilation is detected

--- trunk/sources/volume/core/Rakefile.rb
+++ trunk/sources/volume/core/Rakefile.rb
@@ -1,38 +1,24 @@
 #This code contains fragments from this article:
 #http://aurelianito.blogspot.com/2008/02/compilando-latex-con-rake.html
 
+$LOAD_PATH << "../../"
+
 require "rake/clean"
-
-CLEAN.include(FileList["*.aux", "*.bbl", "*.blg", "*.log"] )
-CLOBBER.include(FileList["*.pdf"] )
-SOURCES = FileList["*.tex", "*.bib", "*.cls", "*.sty", "Rakefile.rb"]
-
-MASTER = "volume-master"
-MASTER_OUTPUT_FILE = MASTER.ext("pdf")
-
-PDFLATEX = "pdflatex"
-BIBTEX = "bibtex"
+require "lib/volumeCommands"
 
 
-def pdflatex(file)
-  sh PDFLATEX, "-halt-on-error", file do 
-    |ok, res|
-    unless ok
-      puts "Error compiling LaTeX file (status=#{res.exitstatus})\n"
-      exit
-    end
-  end
-end
+VOLUME_MASTER = "volume-master"
+VOLUME_MASTER_OUTPUT_FILE = VOLUME_MASTER.ext("pdf")
 
 namespace :volume  do
 
-  file MASTER_OUTPUT_FILE => SOURCES  do
+  file VOLUME_MASTER_OUTPUT_FILE => SOURCES  do
     1.upto 3 do
-      pdflatex MASTER
+      pdflatex VOLUME_MASTER
     end
   end
   
-  task :pdf => MASTER_OUTPUT_FILE
+  task :pdf => VOLUME_MASTER_OUTPUT_FILE
 end
 
 
